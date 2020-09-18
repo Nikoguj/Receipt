49ec7b89d22d2415ef164b0276adc47dad32e31a
Nikoguj <Nikoguj@gmail.com>
2020-09-18 11:55:54 +0200

    Use of the Lombok annotation

--- src/main/java/com/back/receipt/google/domain/GoogleBoundingPoly.java
+++ src/main/java/com/back/receipt/google/domain/GoogleBoundingPoly.java
@@ -1,6 +1,7 @@
 package com.back.receipt.google.domain;
 
 import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -9,22 +10,9 @@ import java.util.List;
 @Getter
 @AllArgsConstructor
 @NoArgsConstructor
+@EqualsAndHashCode
 public class GoogleBoundingPoly {
 
     private List<GoogleVertex> vertices = null;
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-
-        GoogleBoundingPoly that = (GoogleBoundingPoly) o;
-
-        return vertices != null ? vertices.equals(that.vertices) : that.vertices == null;
-    }
-
-    @Override
-    public int hashCode() {
-        return vertices != null ? vertices.hashCode() : 0;
-    }
 }
--- src/main/java/com/back/receipt/google/domain/GoogleResponses.java
+++ src/main/java/com/back/receipt/google/domain/GoogleResponses.java
@@ -2,6 +2,7 @@ package com.back.receipt.google.domain;
 
 import com.back.receipt.google.domain.dto.GoogleTextAnnotationDto;
 import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -10,22 +11,8 @@ import java.util.List;
 @Getter
 @AllArgsConstructor
 @NoArgsConstructor
+@EqualsAndHashCode
 public class GoogleResponses {
 
     private List<GoogleTextAnnotation> textAnnotations = null;
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-
-        GoogleResponses that = (GoogleResponses) o;
-
-        return textAnnotations != null ? textAnnotations.equals(that.textAnnotations) : that.textAnnotations == null;
-    }
-
-    @Override
-    public int hashCode() {
-        return textAnnotations != null ? textAnnotations.hashCode() : 0;
-    }
 }
