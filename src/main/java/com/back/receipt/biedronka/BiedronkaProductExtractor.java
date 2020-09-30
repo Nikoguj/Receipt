a33ce76d031e60a0026466efce643e28d9a74359
Nikoguj <Nikoguj@gmail.com>
2020-09-30 15:34:55 +0200

    Add BiedronkaProductExtractor

---
+++ src/main/java/com/back/receipt/biedronka/BiedronkaProductExtractor.java
@@ -0,0 +1,176 @@
+package com.back.receipt.biedronka;
+
+import com.back.receipt.domain.Product;
+import com.back.receipt.google.domain.*;
+import com.back.receipt.math.LinearFunction;
+import com.back.receipt.math.Quadrangle;
+import com.back.receipt.text.Finder;
+import com.back.receipt.text.Liner;
+import com.back.receipt.text.Text;
+import com.back.receipt.text.TextExtractor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+@Component
+public class BiedronkaProductExtractor {
+
+    @Autowired
+    private Finder finder;
+
+    public List<Product> extract(GoogleResponse googleResponse) {
+
+        GoogleResponses productGoogleResponses = extractProductName(googleResponse);
+
+        List<GoogleTextAnnotation> googleTextAnnotationProducts = Liner.googleLinerWords(productGoogleResponses, googleResponse);
+
+        GoogleResponses productGoogleResponses2 = extractProductPrice(googleResponse);
+
+        List<GoogleTextAnnotation> googleTextAnnotationPrices = Liner.googleLinerWords(productGoogleResponses2, googleResponse);
+
+
+        List<Product> productList = new ArrayList<>();
+
+        int iPrices = 0;
+        for (int i = 0; i < googleTextAnnotationProducts.size(); i++) {
+            String productName = googleTextAnnotationProducts.get(i).getDescription();
+            Product product = new Product(productName);
+            String s1 = googleTextAnnotationPrices.get(iPrices).getDescription();
+            if (i + 1 < googleTextAnnotationProducts.size() && googleTextAnnotationProducts.get(i + 1).getDescription().equals("Rabat")) {
+                String s2 = googleTextAnnotationPrices.get(iPrices + 1).getDescription();
+                String s3 = googleTextAnnotationPrices.get(iPrices + 2).getDescription();
+
+                List<Double> doubleList = dividerPrice(s1);
+                double d1 = doubleList.get(0);
+                double d2 = doubleList.get(1);
+                double d3 = doubleList.get(2);
+
+                s2 = s2.replaceAll("-", "");
+                s2 = s2.replaceAll(",", ".");
+                double d4 = Double.parseDouble(s2);
+
+                s3 = s3.substring(0, s3.length() - 1);
+                s3 = s3.replaceAll(",", ".");
+                double d5 = Double.parseDouble(s3);
+
+                product.setQuantity(d1);
+                product.setPriceForOne(d2);
+                product.setPriceWithoutDiscount(d3);
+                product.setDiscount(d4);
+                product.setPrice(d5);
+
+                i++;
+                iPrices = iPrices + 2;
+            } else {
+                List<Double> doubleList = dividerPrice(s1);
+                double d1 = doubleList.get(0);
+                double d2 = doubleList.get(1);
+                double d3 = doubleList.get(2);
+
+                product.setQuantity(d1);
+                product.setPriceForOne(d2);
+                product.setPrice(d3);
+            }
+            productList.add(product);
+            iPrices++;
+        }
+
+        return productList;
+    }
+
+    private List<Double> dividerPrice(String text) {
+        int space1 = text.indexOf(" ");
+        String s1 = text.substring(0, space1);
+        String s2 = text.substring(space1 + 1, text.length());
+        int space2 = s2.indexOf(" ");
+        s2 = s2.substring(0, space2);
+        String s3 = text.substring(space1 + space2 + 2, text.length());
+        s2 = s2.substring(1);
+        s3 = s3.substring(0, s3.length() - 1);
+
+        s1 = s1.replaceAll(",", ".");
+        s2 = s2.replaceAll(",", ".");
+        s3 = s3.replaceAll(",", ".");
+
+        Double d1 = Double.parseDouble(s1);
+        Double d2 = Double.parseDouble(s2);
+        Double d3 = Double.parseDouble(s3);
+
+        return new ArrayList<Double>(Arrays.asList(d1, d2, d3));
+
+    }
+
+    private GoogleResponses extractProductPrice(GoogleResponse googleResponse) {
+        GoogleBoundingPoly receiptBoundingPoly = finder.getBoundingPolyWord(googleResponse, "PARAGON");
+        GoogleBoundingPoly soldABoundingPoly = finder.getThirdBoundingPolyAfterWord(googleResponse, "SPRZEDAZ OPODATKOWANA A");
+        GoogleBoundingPoly fiscalBoundingPoly = finder.getBoundingPolyWord(googleResponse, "FISKALNY");
+
+        LinearFunction linearFunction5 = new LinearFunction();
+        linearFunction5.calculateAB(fiscalBoundingPoly.getVertices().get(2), receiptBoundingPoly.getVertices().get(3));
+
+        LinearFunction linearFunction6 = new LinearFunction();
+        linearFunction6.calculateAB(googleResponse.getGoogleResponsesList().get(0).getTextAnnotations().get(0).getBoundingPoly().getVertices().get(1),
+                googleResponse.getGoogleResponsesList().get(0).getTextAnnotations().get(0).getBoundingPoly().getVertices().get(2));
+
+        LinearFunction linearFunction7 = new LinearFunction();
+        linearFunction7.calculateAB(soldABoundingPoly.getVertices().get(0), soldABoundingPoly.getVertices().get(1));
+
+        LinearFunction linearFunction8 = new LinearFunction();
+        linearFunction8.calculateAB(fiscalBoundingPoly.getVertices().get(0), fiscalBoundingPoly.getVertices().get(3));
+
+        List<LinearFunction> linearFunctionList2 = new ArrayList<>(Arrays.asList(
+                linearFunction5, linearFunction6, linearFunction7, linearFunction8
+        ));
+
+        Quadrangle quadrangle2 = new Quadrangle(linearFunctionList2, googleResponse);
+        quadrangle2.calculatePointsBoundingPoly();
+        quadrangle2.sortGoogleBoundingPoly();
+
+        GoogleResponses productGoogleResponses2 = TextExtractor.getGoogleResponsesFromQuadrangle(quadrangle2, googleResponse);
+
+        Text.deleteWord(productGoogleResponses2, "FISKALNY");
+        return productGoogleResponses2;
+    }
+
+    private GoogleResponses extractProductName(GoogleResponse googleResponse) {
+        GoogleBoundingPoly receiptBoundingPoly = finder.getBoundingPolyWord(googleResponse, "PARAGON");
+        GoogleBoundingPoly soldABoundingPoly = finder.getThirdBoundingPolyAfterWord(googleResponse, "SPRZEDAZ OPODATKOWANA A");
+        GoogleBoundingPoly soldBBoundingPoly = finder.getThirdBoundingPolyAfterWord(googleResponse, "SPRZEDAZ OPODATKOWANA B");
+
+        if(soldBBoundingPoly == null) {
+            soldBBoundingPoly = finder.getThirdBoundingPolyAfterWord(googleResponse, "SPRZEDAZ OPODATKOWANA C");
+        }
+
+        LinearFunction linearFunction1 = new LinearFunction();
+        linearFunction1.calculateAB(receiptBoundingPoly.getVertices().get(2), receiptBoundingPoly.getVertices().get(3));
+
+        LinearFunction linearFunction2 = new LinearFunction();
+        linearFunction2.calculateAB(soldABoundingPoly.getVertices().get(2), soldBBoundingPoly.getVertices().get(2));
+
+        LinearFunction linearFunction3 = new LinearFunction();
+        linearFunction3.calculateAB(soldABoundingPoly.getVertices().get(0), soldABoundingPoly.getVertices().get(1));
+
+        LinearFunction linearFunction4 = new LinearFunction(1000, 0);
+
+        List<LinearFunction> linearFunctionList = new ArrayList<>(Arrays.asList(
+                linearFunction1, linearFunction2, linearFunction3, linearFunction4
+        ));
+
+        Quadrangle quadrangle = new Quadrangle(linearFunctionList, googleResponse);
+        quadrangle.calculatePointsBoundingPoly();
+        quadrangle.sortGoogleBoundingPoly();
+
+        GoogleResponses productGoogleResponses = TextExtractor.getGoogleResponsesFromQuadrangle(quadrangle, googleResponse);
+
+        Text.deleteWord(productGoogleResponses, "A");
+        Text.deleteWord(productGoogleResponses, "OPODATKOWANA");
+        Text.deleteWord(productGoogleResponses, "SPRZEDAZ");
+        Text.deleteWord(productGoogleResponses, "PARAGON");
+        return productGoogleResponses;
+    }
+
+
+}
---
+++ src/test/java/com/back/receipt/biedronka/BiedronkaProductExtractorTest.java
@@ -0,0 +1,80 @@
+package com.back.receipt.biedronka;
+
+import com.back.receipt.converter.ImageConverter;
+import com.back.receipt.domain.Product;
+import com.back.receipt.google.GoogleClient;
+import com.back.receipt.google.domain.GoogleResponse;
+import com.back.receipt.google.mapper.GoogleMapper;
+import org.junit.Assert;
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BiedronkaProductExtractorTest {
+
+    @Autowired
+    private GoogleClient googleClient;
+
+    @Autowired
+    private GoogleMapper googleMapper;
+
+    @Autowired
+    private BiedronkaProductExtractor biedronkaProductExtractor;
+
+    @Autowired
+    private ImageConverter imageConverter;
+
+    private static String exampleImagePath = "exampleImage/paragBiedro.jpeg";
+
+    @Test
+        public void extract() throws IOException {
+        //Given
+        ClassLoader classLoader = getClass().getClassLoader();
+        File file = new File(classLoader.getResource(exampleImagePath).getFile());
+        GoogleResponse googleResponse = googleMapper.mapToGoogleResponse(googleClient.getGoogleDto(imageConverter.encodeToString(file)));
+
+        Product product1 = new Product("Jaja kl M x10szt C", 3.0, 3.99, 0, 0, 11.97);
+        Product product2 = new Product("Cukier Biały 1kg B", 1.0, 2.49, 0, 0, 2.49);
+        Product product3 = new Product("Chleb kr 500g", 1.0, 1.59, 0.0, 0.0, 1.59);
+        Product product4 = new Product("JajkoNiespodzLE 20g A", 1.0, 3.69, 0.0, 0.0, 3.69);
+        Product product5 = new Product("Mleko UHT 1,5 11 C", 1.0, 1.95, 0.0, 0.0, 1.95);
+        Product product6 = new Product("Ser Swiatowid 500g C", 1.0, 9.69, 0.0, 0.0, 9.69);
+        Product product7 = new Product("Olej Wyborny 11", 1.0, 4.69, 0.0, 0.0, 4.69);
+        Product product8 = new Product("Marg RAMA Poje 1kg", 1.0, 8.99, 8.99, 4.0, 4.99);
+        Product product9 = new Product("Sok Pom Riviva 21 C", 1.0, 5.35, 0.0, 0.0, 5.35);
+
+        List<Product> exceptedProductList = new ArrayList<>(Arrays.asList(
+                product1,
+                product2,
+                product3,
+                product4,
+                product5,
+                product6,
+                product7,
+                product8,
+                product9
+        ));
+
+        //When
+        List<Product> returnProductList = biedronkaProductExtractor.extract(googleResponse);
+
+        //Then
+        Assert.assertEquals(exceptedProductList.size(), returnProductList.size());
+        for (int i = 0; i < exceptedProductList.size(); i++) {
+            Assert.assertEquals(exceptedProductList.get(i), returnProductList.get(i));
+        }
+        Assert.assertEquals(exceptedProductList, returnProductList);
+
+    }
+
+}
