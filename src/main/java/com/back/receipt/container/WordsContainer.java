8816f99d1baebb961c41cbc13cd6f0c92daab484
Nikoguj <Nikoguj@gmail.com>
2020-10-05 16:21:45 +0200

    add WordsContainer

---
+++ src/main/java/com/back/receipt/container/WordsContainer.java
@@ -0,0 +1,17 @@
+package com.back.receipt.container;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class WordsContainer {
+    public static String paragon = "PARAGON";
+    public static String fiskalny = "FISKALNY";
+    public static String sprzedaz_opodatkowana_a = "SPRZEDAZ OPODATKOWANA A";
+    public static String sprzedaz_opodatkowana_b = "SPRZEDAZ OPODATKOWANA B";
+    public static String sprzedaz_opodatkowana_c = "SPRZEDAZ OPODATKOWANA C";
+    public static String a = "A";
+    public static String opodatkowana = "OPODATKOWANA";
+    public static String sprzedaz = "SPRZEDAZ";
+    public static String rabat = "Rabat";
+    public static String sprzedaz_opodatkowana = "SPRZEDAZ OPODATKOWANA";
+}
